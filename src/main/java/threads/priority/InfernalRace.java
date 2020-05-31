cc4905d05734d398d6d2ad0786c3fa8f242556ce
ligaroba <ligaroba@gmail.com>
2020-05-31 15:53:29 +0300

    Fixing dependacies for derby

--- src/main/java/threads/priority/InfernalRace.java
+++ src/main/java/threads/priority/InfernalRace.java
@@ -5,12 +5,11 @@ import threads.mono_multitaks.multitask.racesimulator.Runner;
 public class InfernalRace {
     public static void main(String[] args) {
         Runner A =new Runner("A");
-        Runner B = new Runner("B");
+        Runner B =new Runner("B");
         A.setPriority(Thread.MAX_PRIORITY);
         B.setPriority(Thread.MIN_PRIORITY);
         System.out.println("Thread Runner " + A.getName() + "Has a priority" +
                 A.getPriority());
-
         System.out.println("Thread Runner " + B.getName() + "Has a priority" +
                 B.getPriority());
 
